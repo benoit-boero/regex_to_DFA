058601c503ab67d8f54cb61b5f12ed6eeb0d16e0
benoit-boero <benoitboero@gmail.com>
2024-11-22 15:10:53 +0100

    Adding very basic main to test the program

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include"fsa.hpp"
+#include"regex.hpp"
+#include<cstdlib>
+#include<string>
+
+int main(int argc, char **argv) {
+	if(argc>1) {
+		Regex *reg1 = new Regex(argv[1]);
+	} else {
+		std::cout << "Usage: regex_to_dfa \"name of input file\"" << std::endl;
+	}	
+}
+
